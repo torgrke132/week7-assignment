9491c4324c754ea3d94e5e96da2fc26cf717ca2f
Julia Vrn <julia.voronetskaja@tktk.ee>
2024-02-01 14:55:43 +0100

    week 7 assignment files

---
+++ app.js
@@ -0,0 +1,21 @@
+const displayResult = document.querySelector(/*your code here*/);
+const btnGetAPrediction = document.querySelector(/*your code here*/);
+
+const options = [
+    'You will fall in love this afternoon.', 
+    'You will win a lottery.',
+    'You will lose your phone. Check your left pocket.',
+    'You will fall asleep on a couch.',
+    'You will be taking a ring to Mordor.',
+    'You will get your letter from Hogwarts.',
+    'Elon Musk will buy TikTok.',
+    'Your crash will be mad at you, or give you a kiss. I am just a programm, I have no idea of what will happen in the future.'
+];
+
+btnGetAPrediction.addEventListener('click', () => {
+
+    /*your code here*/
+
+    const prediction = /*your code here*/;
+    displayResult.innerHTML = /*your code here*/;
+});
